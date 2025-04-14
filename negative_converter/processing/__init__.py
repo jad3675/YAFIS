7753f582bf24094a872790702587b3541cc5afba
jad3675 <jad3675@gmail.com>
2025-04-13 20:04:01 -0400

    Delete negative_converter directory

--- negative_converter/processing/__init__.py
+++
@@ -1,6 +0,0 @@
-# Processing package initialization
-from .photo_presets import PhotoPresetManager
-from .film_simulation import FilmPresetManager
-from .adjustments import ImageAdjustments, AdvancedAdjustments
-from .converter import NegativeConverter
-# Expose other relevant classes/functions if needed
